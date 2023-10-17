a17f74584f01243bf728b7c076721367cc70741c
rohitdevs <sa.rohit1998@gmail.com>
2023-10-17 17:45:06 +0530

    submitting code-Tue Oct 17 2023 17:45:03 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,42 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+  const [todo, setTodo] = React.useState("");
+  const [todoList, setTodoList] = React.useState([]);
+
+
+function implementsubmit(e)
+{
+  e.preventDefault();
+  setTodoList([...todoList,todo]);
+  setTodo("");
 }
 
-export default App
+function implementdelete(idx)
+{
+console.log(idx);
+setTodoList(todoList.filter((val,index)=>index!=idx));
+}
+
+  return (
+    <div>
+      <form onSubmit={implementsubmit}>
+        <input type="text" placeholder="Enter text" onChange={(e)=>setTodo(e.target.value)} value={todo}/>
+        <button type="submit">Add Todo</button>
+      
+      </form>
+
+      {todoList.map((val,idx) => (
+        <ul>
+          <li>
+            {val}
+            <button onClick={()=>implementdelete(idx)}>Delete</button>
+          </li>
+        </ul>
+      ))}
+    </div>
+  );
+};
+
+export default App;
